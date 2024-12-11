b99de367d384c00f0ed50d87f9f97879f04f411e
Swetha B S <swetha@multicorewareinc.com>
2024-12-11 10:23:32 +0000

    Merge upstream develop

--- rocAL-setup.py
+++ rocAL-setup.py
@@ -30,16 +30,16 @@ else:
 
 __copyright__ = "Copyright 2022 - 2024, AMD ROCm Augmentation Library"
 __license__ = "MIT"
-__version__ = "2.6.0"
+__version__ = "2.7.0"
 __email__ = "mivisionx.support@amd.com"
 __status__ = "Shipping"
 
 # error check calls
-def ERROR_CHECK(call):
-    status = call
-    if(status != 0):
-        print('ERROR_CHECK failed with status:'+str(status))
+def ERROR_CHECK(waitval):
+    if(waitval != 0): # return code and signal flags
+        print('ERROR_CHECK failed with status:'+str(waitval))
         traceback.print_stack()
+        status = ((waitval >> 8) | waitval) & 255 # combine exit code and wait flags into single non-zero byte
         exit(status)
 
 # Arguments
@@ -98,7 +98,7 @@ else:
     else:
         print("\nSTATUS: CPU Backend Install\n")
 
-# get platfrom info
+# get platform info
 platformInfo = platform.platform()
 
 # sudo requirement check
@@ -167,12 +167,12 @@ elif "Ubuntu" in os_info_data:
 elif "SLES" in os_info_data:
     linuxSystemInstall = 'zypper -n'
     linuxSystemInstall_check = '--no-gpg-checks'
-    platfromInfo = platfromInfo+'-SLES'
+    platformInfo = platformInfo+'-SLES'
     osUpdate = 'refresh'
 elif "Mariner" in os_info_data:
     linuxSystemInstall = 'tdnf -y'
     linuxSystemInstall_check = '--nogpgcheck'
-    platfromInfo = platfromInfo+'-Mariner'
+    platformInfo = platformInfo+'-Mariner'
     osUpdate = 'makecache'
 else:
     print("\nrocAL Setup on "+platformInfo+" is unsupported\n")
@@ -336,11 +336,11 @@ else:
                         ' '+linuxSystemInstall_check+' install -y '+ rocmRPMPackages[i]))
             
     # rocDecode - TBD: Revert when rocDecode is fully supported on all OS
-    # if "Ubuntu" in platfromInfo:
+    # if "Ubuntu" in platformInfo:
         # for i in range(len(rocdecodeDebianPackages)):
             # ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall +
                         # ' '+linuxSystemInstall_check+' install -y '+ rocdecodeDebianPackages[i]))
-    # elif "redhat-7" not in platfromInfo:
+    # elif "redhat-7" not in platformInfo:
         #for i in range(len(rocdecodeRPMPackages)):
             # ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall +
                         # ' '+linuxSystemInstall_check+' install -y '+ rocdecodeRPMPackages[i]))
@@ -379,6 +379,10 @@ else:
     elif "SLES" in platformInfo:
         ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall+' '+linuxSystemInstall_check +
                         ' install dlpack-devel'))
+    elif "redhat" in platformInfo:
+        # no package avialable -- using source
+        ERROR_CHECK(os.system('sudo '+linuxFlag+' '+linuxSystemInstall+' '+linuxSystemInstall_check +
+                        ' install https://rpmfind.net/linux/opensuse/tumbleweed/repo/oss/x86_64/dlpack-devel-0.8-1.5.x86_64.rpm'))
 
 
     # RapidJSON - Source TBD: Package install of RapidJSON has compile issues - https://github.com/Tencent/rapidjson.git -- master
